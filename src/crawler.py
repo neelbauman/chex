4421b911a0a54fa537aee9265668ab06c93dacba
Neel Bauman <neel.bauman@example.com>
2023-08-29 16:53:59 +0900

    make Site class, SiteData class, Contents Class

--- src/crawler.py
+++ src/crawler.py
@@ -12,8 +12,9 @@ Recconaissance Unit
 from bs4 import BeautifulSoup as bs4
 import requests
 
-import os, sys
-import json, re, collections, random, urllib, hashlib, dataclasses, datetime
+import os, sys, urllib, hashlib
+import json, re, datetime, random, collections, dataclasses
+
 
 @dataclasses.dataclass
 class Contents:
@@ -31,28 +32,30 @@ class HrefList:
 
 @dataclasses.dataclass
 class SiteData:
+    """
+    Data Unit
+    """
     first: datetime.datetime
     last: datetime.datetime
     active: bool
     n_refef: int
     n_visited: int
-    contents: Contents
-    hrefs: HrefList
+    hrefs: [HrefList]
     url: str
 
 
 class Site(object):
     """
-    Data Unit
+    SiteData & Contents Unit
     """
-    def __init__(self, data, *args, **kwargs):
+    def __init__(self, data: SiteData, contents: Contents, *args, **kwargs) -> None:
         self._kwargs = kwargs
         self._args = args
         self.data: SiteData = data
-        self.contents = requests.Response()
+        self.contents: Contents = contents
 
     def __str__(self):
-        return self.data["url"]
+        return self.data.url
 
     @property
     def data(self):
@@ -78,6 +81,30 @@ class Site(object):
 
         return property(fget,fset,fdel,doc)
 
+    @property
+    def contents(self):
+        doc = "Site.contents property"
+
+        def fget():
+            return self._contents
+
+        def fset(value):
+            try:
+                self._contents = value
+                return True
+            except:
+                raise ValueError(f"something wrong to set value:{value} for self._contents")
+                return False
+
+        def fdel():
+            try:
+                del self._contents
+                return True
+            except:
+                raise ValueError(f"something wrong to delete self._contents")
+
+        return property(fget, fset, fdel, doc)
+
 
 class Crawler(object):
     """
@@ -87,10 +114,7 @@ class Crawler(object):
         self._domain = domain
         self._args = args
         self._kwargs = kwargs
-        self._target = ""
-        self._next_target = ""
-        self.footprint = []
-        self._init_index(domain)
+        self._init_index()
         self._get_started()
 
     @property
@@ -98,11 +122,11 @@ class Crawler(object):
         doc = "self.index property"
 
         def fget(self):
-            return self.index
+            return self._index
         
         def fset(self, value):
             try:
-                self.index = value
+                self._index = value
                 return True
             except:
                 raise ValueError(f"something wrong to set value:{value} for self.index")
@@ -110,7 +134,7 @@ class Crawler(object):
 
         def fdel(self):
             try:
-                del self.index
+                del self._index
                 return True
             except:
                 raise ValueError(f"something wrong to delete self.index")
@@ -129,7 +153,7 @@ class Crawler(object):
                 self._footprint = value
                 return True
             except:
-                raise ValueError(f"something wrong to set value:{value} for self._footprint")
+                raise ValueError(f"something wrong to set value:{value} for self.footprint")
                 return False
 
         def fdel(self):
@@ -137,7 +161,7 @@ class Crawler(object):
                 del self._footprint
                 return True
             except:
-                raise ValueError(f"something wrong to delete self._footprint")
+                raise ValueError(f"something wrong to delete self.footprint")
 
         return property(fget,fset,fdel,doc)
 
@@ -152,92 +176,103 @@ class Crawler(object):
         
         return name
 
-    def _init_index(self, domain):
+    def _init_index(self):
+        """
+        対象のドメインのindex.jsonがあればそれをロードする。
+        なければディレクトリごとindex.jsonを作成して、自分は空listを持つ。
+        """
         file_name = "index.json"
-        dir_name = "../tmp/" + self._hash_name(self._url_encode(domain), extension="")
-        index_path = dir_name + file_name
-
-        self._dir_name = dir_name
+        dir_name = self._hash_name(self._url_encode(self._domain), extension="")
+        index_path = "../tmp/" + dir_name + file_name
 
         if os.path.exists(index_path):
-            with open(index_path, "r") as f:
-                s = f.read()
-            index = json.loads(s)
-            self._index = index
+            index = self.load_json(index_path)
+            data_list = []
+            for data in index:
+               data_list.append(SiteData(**data))
+
+            self.index = data_list
         else:
-            os.makedirs(dir_name)
+            try:
+                os.makedirs(dir_name)
+            except:
+                raise valueError(f"couldn't make dir:{dir_name}")
+
             with open(index_path, "w") as f:
                 f.write("")
-            self._index = []
-
-    def _get_lp(self):
+            self.index = []
+    
+    def _get_lp(self) -> SiteData:
         """
         サーバールートからリダイレクトされるページ(LP)の取得
         """
         res = self.get_res(self._domain)
-        data = self.get_json_from_res(res)
-        lp_url = res.url
-        self._lp_url = lp_url
+        lp = self._get_data_from_res(res)
 
-        return lp_url, data
+        self._lp_url = lp.url
+
+        return lp
 
     def _get_started(self):
-        n = len(self._index)
+        """
+        self.index:[SiteData]が空であればSiteData.hrefsから次のtargetを決められないので、
+        まずはLPのSiteDataをself.indexに追加する。
+        空でなければ、self.indexの中からランダムでスタート地点を決め、
+        スタート地点のhrefsから次のターゲットをアルゴリズムに従って選定する。
+        """
+        n = len(self.index)
         if n == 0:
-            lp_url, lp_data = self._get_lp()
-            self._index.append(lp_data)
-            self._target = lp_url
-            self._footprint.append(lp_url)
+            data = self._get_lp()
+            self.index.append(data)
         else:
             r = random.randrange(n)
-            data = self._index[r]
-            res = self.get_res(data["url"])
-            data = self.get_json_from_res(res)
+            data = self.index[r]
 
-    def _next_target(self):
-        try:
-            i = self._index.index(self._target)
-        except:
-            raise ValueError("現在のtargetのindexが取得できません。")
+        self._target = data
+        self.footprint = [data.url]
+        self._next_target_url = self._get_next_target_url()
+
+    def _get_next_target_url(self, algorithm:int = 0) -> str:
+        """
+        現在のself._target.hrefsの中から適当なアルゴリズムに従ってself._next_targetを決める。
         
-        hrefs = self._index[i]["hrefs"]
-        n = len(hrefs)
+        0. hrefsのなかからランダムな選択
+        # 1. 頻度の少ないものから。候補が複数ある場合はランダム
+        """
+        if algoritm == 0:
+            hrefs = self._target.hrefs
+            n = len(hrefs)
 
-        if n < 1:
-            raise ValueError("遷移先の候補がもうありません。")
-        else:
-            r = random.randrange(n)
+            if n < 1:
+                raise ValueError("couldn't get next_target")
+            else:
+                r = random.randrange(n)
 
-        while target == next_target:
-            r = random.randrange(n)
-            if self._index[r][
-            next_target = self._index[r]["url"]
-            
+            next_target_url = hrefs[r].url
 
-    def load(self, file_path):
+        return next_target_url
+
+    def load_json(self, file_path):
         try:
             with open(file_path, "r") as f:
                 s = f.read()
             data = json.loads(s)
         except:
             data = None
+            raise ValueError(f"couldn't load json:{file_path}")
 
         return data
 
-    def get_res(self, target:str, **kwargs):
-        target = self._url_encode(target)
+    def get_res(self, url:str, **kwargs):
+        url = self._url_encode(url)
         params = kwargs if kwargs else None
 
         try:
-            res = requests.get(target, params=params)
+            res = requests.get(url, params=params)
         except:
-            raise ValueError("Request Failed")
-
-        self._target = target
-        self._footprint.append(target)
+            raise ValueError(f"Request Failed. Couldn't get from {url}")
 
         return res
-
 
     def _get_soup(self, res):
         soup = bs4(res.content, "html.parser")
@@ -278,13 +313,10 @@ class Crawler(object):
         
         return data
 
-    def get_json_from_target(self, target):
-        res = self.get_res(target)
-        data = slef.get_json_from_res(res)
+    def _get_data_from_res(self, res):
+        json_dict = self.get_json_from_res(res)
+        data = SiteData(**json_dict)
 
         return data
         
-
-
-
 
